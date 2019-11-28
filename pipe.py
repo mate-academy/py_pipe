64070093b8141d6926db24e95fbd413fee4a0175
logotip <logotip123@yahoo.com>
2019-11-28 12:50:15 +0200

    v 1.0

--- pipe.py
+++ pipe.py
@@ -1,3 +1,38 @@
-def calc(x: float, *funcs: int) -> float:
-    return x
+"""pipe module"""
 
+
+def function0(num):
+    """
+    :param num:
+    :return:
+    """
+    return num * num
+
+
+def function1(num):
+    """
+    :param num:
+    :return:
+    """
+    return num // 2
+
+
+def function2(num):
+    """
+    :param num:
+    :return:
+    """
+    return num + 4
+
+
+def calc(num: float, *funcs: int) -> float:
+    """
+    :param num:
+    :param funcs:
+    :return:
+    """
+    functions = (function0, function1, function2)
+    result = num
+    for item in funcs:
+        result = functions[item](result)
+    return result
