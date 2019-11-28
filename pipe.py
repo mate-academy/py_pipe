b2734a00d08f56b4656d2e9a6816e71df1d86939
DukhDmytro <cowboybebop4991@gmail.com>
2019-11-28 16:45:08 +0200

    2nd commit

--- pipe.py
+++ pipe.py
@@ -1,22 +1,9 @@
 """docstring"""
 
 
-def calc(val: float, *_funcs: int) -> float:
-    """calc"""
-    def zero(inp):
-        return inp * inp
-
-    def first(inp):
-        return inp // 2
-
-    def second(inp):
-        return inp + 4
-    func_order = locals()["_funcs"]
-    for i in func_order:
-        if i == 2:
-            val = second(val)
-        elif i == 1:
-            val = first(val)
-        elif i == 0:
-            val = zero(val)
-    return val
+def calc(value: float, *funcs: int) -> float:
+    """calculate the result of composition of this functions"""
+    funcs_list = [lambda inp: inp * inp, lambda inp: inp // 2, lambda inp: inp + 4]
+    for i in funcs:
+        value = funcs_list[i](value)
+    return value
