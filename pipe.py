6f399c7c2daa27a8c1331e1d92c734cb1768d175
DukhDmytro <cowboybebop4991@gmail.com>
2019-11-28 12:28:22 +0200

    test

--- pipe.py
+++ pipe.py
@@ -1,3 +1,21 @@
+"""docstring"""
+
+
 def calc(x: float, *funcs: int) -> float:
+    """calc"""
+    for i in locals()["funcs"]:
+        if i == 2:
+            a = lambda x: x + 4
+            x = a(x)
+        elif i == 1:
+            b = lambda x: x// 2
+            x = b(x)
+        elif i == 0:
+            c = lambda x: x * x
+            x = c(x)
+
+    print(x)
     return x
 
+
+calc(0, 2, 2, 2)
