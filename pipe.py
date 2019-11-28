6b5c164545361b658ee0d2200e989fd317a9107c
YuPo13 <yuliya.povzlo@gmail.com>
2019-11-28 12:32:19 +0200

    Good

--- pipe.py
+++ pipe.py
@@ -1,3 +1,32 @@
-def calc(x: float, *funcs: int) -> float:
-    return x
+"""module docstring"""
 
+def zero(starting_point):
+    """func docstring"""
+    return starting_point ** 2
+
+
+def one(starting_point):
+    """func docstring"""
+    return starting_point // 2
+
+
+def two(starting_point):
+    """func docstring"""
+    return starting_point + 4
+
+
+def calc(starting_point, *func_order):
+    """func docstring"""
+    func_order = [*func_order]
+    result = starting_point
+    for item in func_order:
+        if item == 0:
+            result = zero(result)
+        elif item == 1:
+            result = one(result)
+        elif item == 2:
+            result = two(result)
+        else:
+            raise ValueError
+
+    return result
