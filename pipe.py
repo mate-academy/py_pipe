ce43d3d393c2bb51074f26605e72bcb711269629
vlysenok <tina.lysenok@gmail.com>
2019-11-28 16:33:24 +0200

    hw9_py_pype

--- pipe.py
+++ pipe.py
@@ -1,3 +1,29 @@
-def calc(x: float, *funcs: int) -> float:
-    return x
+"""
+module pipe
+"""
 
+
+def calc(num: float, *funcs: int) -> float:
+    """
+    You have three functions:
+    f0: x*x
+    f1: x//2
+    f2: x + 4
+
+    For given x and sequence of functions numbers calculate
+    the result of composition of this functions.
+    Example: (0, 1, 2) -> x = f2(f1(f0(x)))
+    :param num: float
+    :param funcs: int
+    :return: float
+    """
+    func_dict = {
+        0: lambda x: x * x,
+        1: lambda x: x // 2,
+        2: lambda x: x + 4
+    }
+
+    result_num = num
+    for func in funcs:
+        result_num = func_dict[func](result_num)
+    return result_num
