23a3372a1ae90e9541b3eae82464d2a1db353da3
YuPo13 <yuliya.povzlo@gmail.com>
2019-11-30 18:21:58 +0200

    Re-written according to feedback obtained

--- pipe.py
+++ pipe.py
@@ -17,16 +17,10 @@ def two(starting_point):
 
 def calc(starting_point, *func_order):
     """func docstring"""
-    func_order = [*func_order]
+    order = func_order
     result = starting_point
-    for item in func_order:
-        if item == 0:
-            result = zero(result)
-        elif item == 1:
-            result = one(result)
-        elif item == 2:
-            result = two(result)
-        else:
-            raise ValueError
+    functions = [zero, one, two]
+    for item in order:
+        result = functions[item](result)
 
     return result
